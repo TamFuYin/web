d4913bf790c96646fe75a8871a322bd8e606ddf2
tamfuyin <tamfuyin@gmail.com>
2025-11-18 07:37:50 +0800

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import Time from './components/Time'
+import { Noto_Sans_SC} from 'next/font/google'
+
+const notoSansSC = Noto_Sans_SC({ weight: '400', subsets: ['latin'] })
 
 export const metadata = {
   title: '云剪贴板',
@@ -9,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh">
-      <body>
+      <body className={notoSansSC.className}>
         <header style={{ padding: '1rem', backgroundColor: '#f0f0f0' }}>
           当前时间: <Time />
         </header>
